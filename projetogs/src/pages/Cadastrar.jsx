a3ed3765dc43992c76e2d09a85e5e08eb820ca6b
Lucas Serrano Rocco <156000513+lucasserrano10@users.noreply.github.com>
2024-11-21 13:38:18 -0300

    login, cadastro e readme done

--- projetogs/src/pages/Cadastrar.jsx
+++ projetogs/src/pages/Cadastrar.jsx
@@ -3,12 +3,92 @@ import { useFetch } from "../hooks/useFetch";
 import FaixaAzul from "../components/FaixaAzul";
 import styled from "styled-components";
 
-// Estilo do container
+
 const Container = styled.div`
   padding-top: 50px;
 `;
 
+const Formulario = styled.div`
+  background-color: white;
+  margin: auto;
+  width: 700px;
+  border-radius: 25px;
+  padding: 25px;
+  text-align: center;
+  margin-top: 40px;
+
+  h1 {
+    letter-spacing: 0.8px;
+    font-size: 1.9rem;
+    font-weight: 800;
+    font-family: poppins;
+  }
+
+  p {
+    font-size: 1.2rem;
+    font-weight: 900;
+    padding: 10px;
+    font-family: "Doto";
+  }
+
+  form {
+    margin-top: 20px;
+    display: flex;
+    flex-direction: column;
+    gap: 15px;
+
+    input {
+      width: 100%;
+      padding: 10px;
+      border: 1px solid #ccc;
+      border-radius: 5px;
+      font-size: 1rem;
+    }
+
+    button {
+      background-color: #4caf50;
+      color: white;
+      border: none;
+      padding: 10px;
+      border-radius: 5px;
+      font-size: 1.1rem;
+      cursor: pointer;
+    }
+
+    button:hover {
+      background-color: #45a049;
+    }
+  }
+`;
+
+const TextoVerde = styled.span`
+  color: #1ABC9C;
+`;
+
 const Cadastrar = () => {
+  const [formData, setFormData] = useState({
+    nome: "",
+    email: "",
+    idade: "",
+    telefone: "",
+  });
+
+  const { httpConfig, loading, error } = useFetch("http://localhost:3000/usuarios");
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    httpConfig(formData, "POST"); 
+    alert("Cadastro realizado com sucesso!");
+    setFormData({ nome: "", email: "", idade: "", telefone: "" }); 
+  };
   return (
     <>
       <Container>
@@ -17,6 +97,49 @@ const Cadastrar = () => {
           paragrafo="Cadastre-se e receba conteúdo semanal do nosso newsletter sobre nossa revolução energética que faremos juntos com esse incrível projeto!"
         />
       </Container>
+      <Formulario>
+        <h1>Faça o Cadastro</h1>
+        <p>Faça o cadastro e receba informações semanalmente por meio do nosso <TextoVerde>NewsLetter</TextoVerde>
+        </p>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            name="nome"
+            placeholder="Nome completo"
+            value={formData.nome}
+            onChange={handleChange}
+            required
+          />
+          <input
+            type="email"
+            name="email"
+            placeholder="E-mail"
+            value={formData.email}
+            onChange={handleChange}
+            required
+          />
+          <input
+            type="number"
+            name="idade"
+            placeholder="Idade"
+            value={formData.idade}
+            onChange={handleChange}
+            required
+          />
+          <input
+            type="number"
+            name="telefone"
+            placeholder="Número de Telefone"
+            value={formData.telefone}
+            onChange={handleChange}
+            required
+          />
+          <button type="submit" disabled={loading}>
+            {loading ? "Enviando..." : "Cadastrar"}
+          </button>
+        </form>
+        {error && <p style={{ color: "red" }}>Erro ao cadastrar: {error}</p>}
+      </Formulario>
     </>
   );
 };
--- projetogs/src/pages/Login.jsx
+++ projetogs/src/pages/Login.jsx
@@ -1,18 +1,125 @@
-import styled from "styled-components"
-import FaixaAzul from "../components/FaixaAzul"
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import FaixaAzul from "../components/FaixaAzul";
+import CryptoJS from 'crypto-js';
 
 const Container = styled.div`
-    padding-top:50px;
+    padding-top: 50px;
+`;
+
+const ContainerLogin = styled.div`
+    background-color: white;
+    width: 50vw;
+    height: 60vh;
+    margin: 30px auto;
+    border-radius: 25px;
+    box-shadow: 3px 3px 3px black;
+    padding: 20px;
+
+    h1 {
+        text-align: center;
+        font-size: 24px;
+        font-family: poppins;
+        font-weight: 900;
+    }
+
+    p {
+        text-align: center;
+        font-size: 16px;
+        font-family: "Sour-gummy";
+        font-weight: 500;
+    }
 `;
 
 const Login = () => {
-    return (
-    <>
-    <Container>
-        <FaixaAzul titulo="ÁREA DE LOGIN" paragrafo="Faça o Login para continuar recebendo nossas novidades !"></FaixaAzul>
-    </Container>
-    </>
-    )
-}
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-export default Login
+
+    const handleLogin = () => {
+        const encryptedEmail = CryptoJS.AES.encrypt(email, 'secret-key').toString();
+        const encryptedPassword = CryptoJS.AES.encrypt(password, 'secret-key').toString();
+
+        localStorage.setItem('userEmail', encryptedEmail);
+        localStorage.setItem('userPassword', encryptedPassword);
+
+        setIsLoggedIn(true);
+        alert('Login bem-sucedido!');
+    };
+    const handleLogout = () => {
+        localStorage.removeItem('userEmail');
+        localStorage.removeItem('userPassword');
+        setIsLoggedIn(false);
+        alert('Logout bem-sucedido!');
+    };
+
+    return (
+        <>
+            <Container>
+                <FaixaAzul
+                    titulo="ÁREA DE LOGIN"
+                    paragrafo="Faça o Login para continuar recebendo nossas novidades!"
+                />
+            </Container>
+            <ContainerLogin>
+                <h1>Área de Login</h1>
+                <p>Faça seu login em nosso site e continue fazendo parte da maior revolução energética já vista</p>
+
+                {!isLoggedIn ? (
+                    <div>
+                        <input
+                            type="email"
+                            placeholder="E-mail"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            style={{ width: '100%', padding: '10px', margin: '10px 0' }}
+                        />
+                        <input
+                            type="password"
+                            placeholder="Senha"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            style={{ width: '100%', padding: '10px', margin: '10px 0' }}
+                        />
+                        <button
+                            onClick={handleLogin}
+                            style={{
+                                width: '100%',
+                                padding: '10px',
+                                backgroundColor: '#4CAF50',
+                                color: 'white',
+                                border: 'none',
+                                borderRadius: '5px',
+                                cursor: 'pointer',
+                            }}
+                        >
+                            Login
+                        </button>
+                    </div>
+                ) : (
+                    <div>
+                        <p>Bem-vindo! Você está logado.</p>
+                        <button
+                            onClick={handleLogout}
+                            style={{
+                                width: '100%',
+                                padding: '10px',
+                                backgroundColor: 'red',
+                                color: 'white',
+                                border: 'none',
+                                marginTop: '30px',
+                                borderRadius: '5px',
+                                cursor: 'pointer',
+                            }}
+                        >
+                            Logout
+                        </button>
+                    </div>
+                )}
+            </ContainerLogin>
+        </>
+    );
+};
+
+export default Login;
